7d1498245e50f466d4f4d27fabd0d9eb2b589c0e
Elina Kettunen <elina.kettunen@iki.fi>
2023-05-02 13:46:13 +0300

    added update flag

--- data/sync_data.sh
+++ data/sync_data.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-rsync -arv --exclude '~$*' /Volumes/h840/mira/MIRA2/analysis\ data\ model/data/RAW/ RAW
+rsync -arv --update --exclude '~$*' /Volumes/h840/mira/MIRA2/analysis\ data\ model/data/RAW/ RAW
